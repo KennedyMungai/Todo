1a104399e999327b7c2cc06430d9034aca485423
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-24 20:08:25 +0300

    Added the basic setup code for the context

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -1,1 +1,13 @@
-namespace 
+using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Data;
+
+public class TodoContext : DbContext
+{
+    public DbSet<Todo>? todo { get; set; }
+    
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+        
+    }
+}
